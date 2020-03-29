08f06a744d537574770426a6571a0ddb922bd26d
eminguyen <emilyhuongnguyen@gmail.com>
2020-03-29 16:05:49 -0700

    Connect to backend

--- frontend/src/actions/wordActions.js
+++ frontend/src/actions/wordActions.js
@@ -1,3 +1,25 @@
+import Config from '../config';
+
+import { replace } from 'connected-react-router';
+
 export const submitWord = (word) => async dispatch => {
-  console.log(word);
-}
+  try {
+    const response = await fetch(Config.API_URL + `/flashcard?search=${word}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    });
+
+    const data = await response.json();
+
+    if (!data) throw new Error('Empty response from server');
+    if (data.error) throw new Error(data.error.message);
+    console.log('here');
+    console.log(data)
+
+  } catch (error) {
+    console.error('uh oh')
+  }
+};
